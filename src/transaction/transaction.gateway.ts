68f646dc9b6186e480a2efd72ecc228669c79b13
Chi Viet <chiviet296@gmail.com>
2024-09-07 15:25:06 +0700

    remove web socket

--- src/transaction/transaction.gateway.ts
+++ src/transaction/transaction.gateway.ts
@@ -1,20 +1,20 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+// import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+// import { Server } from 'socket.io';
 
-@WebSocketGateway({
-  cors: {
-    origin: [
-      'https://ton-shop.onrender.com',
-      'https://ton-shop.onrender.com/transactionStatus',
-      'https://tonshop-be.onrender.com',
-    ],
-  },
-})
-export class TransactionGateway {
-  @WebSocketServer()
-  server: Server;
+// @WebSocketGateway({
+//   cors: {
+//     origin: [
+//       'https://ton-shop.onrender.com',
+//       'https://ton-shop.onrender.com/transactionStatus',
+//       'https://tonshop-be.onrender.com',
+//     ],
+//   },
+// })
+// export class TransactionGateway {
+//   @WebSocketServer()
+//   server: Server;
 
-  notifyTransactionStatus(status: any) {
-    this.server.emit('transactionStatus', status);
-  }
-}
+//   notifyTransactionStatus(status: any) {
+//     this.server.emit('transactionStatus', status);
+//   }
+// }

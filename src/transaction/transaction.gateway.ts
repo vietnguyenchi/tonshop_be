fd29284afe7eb523ec66ae4565bfe45fad4824c1
Chi Viet <chiviet296@gmail.com>
2024-09-12 15:07:02 +0700

    test socket message

--- src/transaction/transaction.gateway.ts
+++ src/transaction/transaction.gateway.ts
@@ -3,16 +3,29 @@ import {
   WebSocketServer,
   SubscribeMessage,
   ConnectedSocket,
+  MessageBody,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: 'https://ton-shop.onrender.com',
+  },
+})
 export class TransactionGateway {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('transactionStatus')
-  notifyTransactionStatus(data: any, userId: string) {
-    this.server.to(userId).emit('transactionStatus', data);
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() transactionId: string,
+  ) {
+    client.join(transactionId);
+    console.log(`Client joined room: ${transactionId}`);
+  }
+
+  notifyTransactionStatus(transactionId: string, data: any) {
+    this.server.to(transactionId).emit('transactionStatus', data);
   }
 }

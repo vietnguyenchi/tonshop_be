8163e9480ca97eef770c29f6314c32ae2bd71459
Chi Viet <chiviet296@gmail.com>
2024-09-12 17:17:26 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { Bot } from 'grammy';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://ton-shop.onrender.com',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   await app.listen(3000);
 
   const bot = new Bot(process.env.BOT_TOKEN);

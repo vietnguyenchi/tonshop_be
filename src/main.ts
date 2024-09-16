b2270af1e5f781b864b61867399b9a3ce46f79a8
Chi Viet <chiviet296@gmail.com>
2024-09-16 19:43:40 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
    const app = await NestFactory.create(AppModule);
    app.enableCors({
-      origin: '*',
+      origin: 'https://ton-shop.onrender.com',
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       allowedHeaders: 'Content-Type, Accept, Authorization',
    });
